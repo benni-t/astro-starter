cbbc48321623597e4581307e0dcb858456b458c7
Benjamin Thomsen <info@benjaminthomsen.de>
2024-01-31 17:19:43 +0100

    go

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import mdx from '@astrojs/mdx';
 
-import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel(),
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
 });
